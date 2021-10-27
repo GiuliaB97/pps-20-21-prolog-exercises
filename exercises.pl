4036b4e80c5d62e2e9d5bb07a9573129343703bb
Giulia <giulia.brugnatti@studio.unibo.it@users.noreply.github.com>
2021-10-27 16:03:46 +0200

    update ex

--- exercises.pl
+++ exercises.pl
@@ -1,22 +1,21 @@
 % search(Elem, List)
-search(E, [E]):- !.
-search(E, [H|T]):- search(E, T).
 
 % search2Consecutive(Elem, List)
+
 % search2AnyElemBetween(Elem, List)
 
 % search2Occurence(Elem, List)
 
-%size(List, Size)
+% size(List, Size)
 
 %sizeZero(List, Size)	%test: sizeZero([1,2], s(s(zero))).
-=> RIFARE
 
 %sum(List, Sum)
 
-%max(List, Max)
+% max(List, Max)
 
 % maxMin(List,Max,Min)
+
 % same(List1,List2)
 
 %allBigger(L, L).
@@ -37,25 +36,22 @@ search(E, [H|T]):- search(E, T).
 
 % dropAll(Elem,List,OutList)
 
-%fromList(List, Graph)
+% fromList(List, Graph)
 
-%fromCircList(List, Graph)
+% fromCircList(List, Graph)
 
 % dropNode(Graph, Node, OutGraph); drop all edges starting and leaving from a Node reaching(+Graph, +Node, -List); all the nodes that can be reached in 1 step from Node possibly use findall, looking for e(Node,_) combined with member(?Elem,?List)
 
+% reaching(+Graph, +Node, -List)% all the nodes that can be reached in 1 step from Node possibly use findall, looking for e(Node,_) combined with member(?Elem,?List)
+% test:reaching([e(1,2),e(1,3),e(2,3)],1,L). -> L/[2,3].
 
-%2.4 reaching(+Graph, +Node, -List)% all the nodes that can be reached in 1 step from Node possibly use findall, looking for e(Node,_) combined with member(?Elem,?List)
-%test:reaching([e(1,2),e(1,3),e(2,3)],1,L). -> L/[2,3].
-
-%Advanced exerices
-%2.5 anypath(+Graph, +Node1, +Node2, -ListPath) a path from Node1 to Node2 if there are many path, they are showed 1-by-1
+% anypath(+Graph, +Node1, +Node2, -ListPath) a path from Node1 to Node2 if there are many path, they are showed 1-by-1
 %test: anypath([e(1,2),e(1,3),e(2,3)],1,3,L). %output: – L/[e(1,2),e(2,3)] e L/[e(1,3)]
 
-%2.6 allreaching(+Graph, +Node, -List)
-% all the nodes that can be reached from Node. Suppose the graph is NOT circular!. Use findall and anyPath!
-%test: allreaching([e(1,2),e(2,3),e(3,5)],1,[2,3,5]).
+% allreaching(+Graph, +Node, -List)  all the nodes that can be reached from Node. Suppose the graph is NOT circular!. Use findall and anyPath!
+% test: allreaching([e(1,2),e(2,3),e(3,5)],1,[2,3,5]).
 
-%2.7 grid-like nets
+% grid-like nets
 %During last lesson we see how to generate a gridlike network. Adapt that code to create a graph for the predicates implemented so far. Try to generate all paths from a node to another,limiting the maximum number of hops
 
 %EXTRA
