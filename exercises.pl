c655886c5df95f685282937c733657252162dd77
GiuliaB97 <60556203+GiuliaB97@users.noreply.github.com>
2021-11-17 13:23:41 +0100

    Update exercises.pl

--- exercises.pl
+++ exercises.pl
@@ -1,46 +1,68 @@
 % search(Elem, List)
+%test: Yes: search(3, [4,5 ,2, 3, 5]). 		No:search(32, [4,5 ,2, 3, 5]).
 
 % search2Consecutive(Elem, List)
+%test: Yes: search2Consecutive(3, [4,5 ,2, 3,3, 5]).		No search2Consecutive(3, [4,5 ,2, 3,9, 3, 5]).
 
 % search2AnyElemBetween(Elem, List)
+%test: Yes search2AnyElemBetween(3, [4,5 ,2, 3,0, 3, 5]).	No: search2AnyElemBetween(3, [4,5 ,2, 3, 3, 5]).
 
 % search2Occurence(Elem, List)
+%test: Yes: search2Occurence(3, [4,5 ,2, 3,9, 3, 5]). 		No: search2Occurence(3, [4,5 ,2, 3,9, 3, 5]).
+
+% sizeNaturalNumber(List, Size)
+%test: Yes sizeNaturalNumber([1,2], s(s(zero))).		No: sizeNaturalNumber([1,2], s(zero)).
 
 % size(List, Size)
-
-% sizeZero(List, Size)
+%test: Yes: size([1,2], 2).		No: size([1,2], 5).
 
 % sum(List, Sum)
+%test: Yes: sum([1,2], 3).		No:  sum([1,2], 6).
 
 % max(List, Max)
+%test: Yes: max([1,2, 0], 2).		No:  max([1,2, 0], 1)
 
 % maxMin(List,Max,Min)
+%test: Yes: maxMin([1,2, 9, 0], 9, 0). 		No: maxMin([1,2, 9, 0], 3, 0).
 
 % same(List1,List2)
+%test: Yes:same([1, 2], [1,2]).		No: same([1, 2], [0,1,2]).
 
 % allBigger(L, L).
+%test: Yes:allBigger([11, 22, 33], [0,1,2]).	 No:allBigger([11,0, 22, 33], [0,1,2]).
 
 % sublist(List1,List2)
+%test: Yes: sublist([1,2,3,4],[2,3]).		No: sublist([1,2,3,4],[2,3, 5]).
 
 % seq(N,List)
+%test: Yes: seq(2, [0,0]). 	No: seq(2, [0,0,0]).
 
 % seqR(N,List)
+% test: Yes: seqR(3,[3,2,1,0]).		No: seqR(3, [3,2,1]).
 
-% seqR(N,List)
+% seqL(N,List)
+% test: Yes: seqL(3,[0,1,2,3])		No: seqL(3, [1, 2,3]).
 
 % dropAny(Elem,List,OutList)
+%test: Yes: dropAny(3,[0,1,2,3,7], [0,1,2,7]).		No: dropAny( 3,[0,1,2,3,7], [0,1,2,3,7]).
 
 % dropFirst(Elem,List,OutList)
+%test: Yes: dropFirst( 3,[0,1,2,3,7,3], [0,1,2,7,3]).		No: dropFirst( 3,[0,1,2,3,7,3], [0,1,2,3,7,3]).
 
 % dropLast(Elem,List,OutList)
+%test: Yes: dropLast( 3,[0,1,2,3,7,3], [0,1,2,3,7]).		No: dropLast( 3,[0,1,2,3,7,3], [0,1,2,3,7,3]).
 
 % dropAll(Elem,List,OutList)
+%test: Yes: dropAll( 3,[0,1,2,3,7,3], [0,1,2,7]).		No: dropLast( 3,[0,1,2,3,7,3], [0,1,2,3,7,3]).
 
-% fromList(List, Graph)
+%fromList(List, Graph)
+%test Yes: fromList([10,20,30],[e(10,20),e(20,30)]).
 
-% fromCircList(List, Graph)
+%fromCircList(List, Graph)
+%test: Yes: fromCircList([10,20,30],[e(10,20),e(20,30),e(30,10)]).
 
-% dropNode(Graph, Node, OutGraph); drop all edges starting and leaving from a Node reaching(+Graph, +Node, -List); all the nodes that can be reached in 1 step from Node possibly use findall, looking for e(Node,_) combined with member(?Elem,?List)
+% dropNode(+Graph, +Node, -OutGraph); drop all edges starting and leaving from a Node
+%test dropNode([e(1,2),e(1,3),e(2,3)],1,[e(2,3)]).
 
 % reaching(+Graph, +Node, -List)% all the nodes that can be reached in 1 step from Node possibly use findall, looking for e(Node,_) combined with member(?Elem,?List)
 % test:reaching([e(1,2),e(1,3),e(2,3)],1,L). -> L/[2,3].
@@ -51,13 +73,10 @@
 % allreaching(+Graph, +Node, -List)  all the nodes that can be reached from Node. Suppose the graph is NOT circular!. Use findall and anyPath!
 % test: allreaching([e(1,2),e(2,3),e(3,5)],1,[2,3,5]).
 
-% grid-like nets
-%During last lesson we see how to generate a gridlike network. Adapt that code to create a graph for the predicates implemented so far. Try to generate all paths from a node to another,limiting the maximum number of hops
-
 %EXTRA
 
-%inv
-%test: inv([0,1],[1,0])
+%reverse(List, OutList)
+%test: Yes: reverse([1,2, 3, 4],[4, 3,2,1]). No: reverse([1,2, 3, 4],[4, 2,1]).
 
 
 % double
@@ -65,7 +84,7 @@
 
 
 % times
-% test: times([0], 3, [0,0,0])
+%test: times([0], 3, [0,0,0]). 	No: times([0], 2, [0,0,0]).
 
-% proj
-% test: proj([[a,c,d],[b]],[a,b])
+%listFirsts(List1, List2, OutList)
+%test: listsFirst([[a,c,d],[b]],[a,b])
